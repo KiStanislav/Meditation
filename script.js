2ee47918cddf22676d8d37823333c0117ccfdefd
Stanislav <stif@tut.by>
2021-04-18 15:01:36 +0300

    refactor: clear code

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const video = document.querySelector('.video-container video');
 const sounds = document.querySelectorAll('.container__sounds button');
 const timeDisplay = document.querySelector('.time-display');
 const timeSelect = document.querySelectorAll('.time-select');
-const outlineLenght = outline.getTotalLength();
+const outlineLength = outline.getTotalLength();
 const ZERO = '0';
+const TEN = 10;
+const SECONDS_PER_MINUTE = 60;
 let fakeTime = 600;
 
-outline.style.strokeDasharray = outlineLenght;
-outline.style.strokeDashoffset = outlineLenght;
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
 
 const checkPlaying = song => {
   if(song.paused) {
     song.play();
     play.src = 'svg/pause.svg';
     video.play();
-  }else {
+  } else {
     song.pause();
     play.src = 'svg/play.svg';
     video.pause();
@@ -27,20 +29,20 @@ const checkPlaying = song => {
 play.addEventListener('click', () => checkPlaying(song));
 
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? ZERO : '') + n;
+  return (parseInt(n, 10) < TEN ? ZERO : '') + n;
 }
 
 timeSelect.forEach(option => {
   option.addEventListener('click', function(){
-    fakeTime = this.getAttribute('data-time');
-    timeDisplay.textContent = `${addZero(Math.floor(fakeTime / 60))}:${addZero(Math.floor(fakeTime % 60))}`
+    fakeTime = this.dataset.dataTime;
+    timeDisplay.textContent = `${addZero(Math.floor(fakeTime / SECONDS_PER_MINUTE))}:${addZero(Math.floor(fakeTime % SECONDS_PER_MINUTE))}`
   });
 });
 
 sounds.forEach(sound => {
   sound.addEventListener('click', function() {
-    song.src = this.getAttribute('data-sound');
-    video.src = this.getAttribute('data-video');
+    song.src = this.dataset.dataSound;
+    video.src = this.dataset.dataVideo;
     checkPlaying(song);
   } )
 })
@@ -48,9 +50,9 @@ sounds.forEach(sound => {
 song.ontimeupdate = () => {
   let currentTime = song.currentTime;
   let elapsed = fakeTime - currentTime;
-  let seconds = addZero(Math.floor(elapsed % 60));
-  let minutes = addZero(Math.floor(elapsed / 60));
-  let progress = outlineLenght - (currentTime / fakeTime) * outlineLenght;
+  let seconds = addZero(Math.floor(elapsed % SECONDS_PER_MINUTE));
+  let minutes = addZero(Math.floor(elapsed / SECONDS_PER_MINUTE));
+  let progress = outlineLength - (currentTime / fakeTime) * outlineLength;
   outline.style.strokeDashoffset = progress;
   timeDisplay.textContent = `${minutes}:${seconds}`;
   if(currentTime >= fakeTime) {
